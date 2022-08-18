cd5af1f892b600047d681f20c92ab34615befdc6
shadababe04 <shadababe04@gmail.com>
2022-08-18 15:45:45 +0530

    class lec

---
+++ 3class/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+    private:
+    int a,b;
+    public:
+    void set_data(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+    void show_data()
+    {
+        cout<<"a = "<<a<<endl;
+        cout<<"b = "<<b<<endl;
+    }
+
+};
+
+int main()
+{
+    Complex c1;
+    c1.set_data(3,4);
+    c1.show_data();
+
+    return 0;
+}
