5e44d55b1eb38f2e5d9a2f120af619972c4a0f55
shadababe04 <shadababe04@gmail.com>
2022-08-18 15:15:34 +0530

    array

---
+++ 2CPP/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int marks[]= {23,45,55,67};
+    cout<<"These are marks:"<<endl;
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    return 0;
+
+}
