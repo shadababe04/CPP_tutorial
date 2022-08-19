1d34d723c7a2b4b273e40f5298f8d86ae44b2ec0
shadababe04 <shadababe04@gmail.com>
2022-08-19 16:24:21 +0530

    constructor

---
+++ 5constructor/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-19
+ * @
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+    private:
+    int a, b;
+    public:
+/**
+ * @brief Construct a new Complex objectl
+ * 
+ */
+    Complex()
+    {
+        cout<<"Hello Constructor";
+    }
+};
+
+int main()
+{
+    Complex c1,c2,c3;//3 times object means constructor call 3 times
+    return 0;
+}
