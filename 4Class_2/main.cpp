8077b79ae9c2330fbd068229da8cdb5bf1dc8f8c
shadababe04 <shadababe04@gmail.com>
2022-08-19 15:38:22 +0530

    class application

--- 4Class_2/main.cpp
+++ 4Class_2/main.cpp
@@ -17,6 +17,14 @@ class Complex
         cout<<"a = "<<a<<endl;
         cout<<"b = "<<b<<endl;
     }
+    Complex add(Complex c)
+    {
+        Complex temp;
+        temp.a=a+c.a;
+        temp.b=b+c.b;
+        return(temp);
+
+    }
 
 };
 
@@ -25,7 +33,7 @@ int main()
     Complex c1,c2,c3; // c1 is a object
     c1.set_data(3,4);
     c2.set_data(5,8);
-    c3=c1+c2;
+    c3=c1.add(c2);
     c3.show_data();
 
     return 0;
