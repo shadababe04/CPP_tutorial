0396e98ff60c9ec99f542feb40f840a3478523f4
shadababe04 <shadababe04@gmail.com>
2022-08-19 17:16:24 +0530

    constructor with arguments

---
+++ 6constructor_2/main.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file main.cpp
+ * @author shadab (TE434973)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<iostream>
+using namespace std;
+/**
+ * @brief Class Complex
+ * 
+ */
+class Complex
+{
+    private:
+    int a,b;
+    public:
+    /**
+     * @brief Construct a new Complex object
+     * 
+     * @param x 
+     * @param y 
+     */
+    Complex(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+    Complex(int x)
+    {
+        a=x;
+    }
+    Complex()
+    {
+        a=0;
+        b=0;
+    }
+};
+
+/**
+ * @brief main
+ * 
+ * @return int 
+ */
+int main()
+{
+    Complex c1(3,4),c2(5),c3=6,c4;
+    return 0;
+}
